9d838ef76a0bdd478e358cdb749007acab13941f
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2023-07-04 15:35:24 +0300

    Some unuseful data have been deleted.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import {getSortedPostsData} from '../lib/posts';
 import Date from '../components/date';
@@ -18,13 +17,9 @@ export async function getStaticProps() {
 export default function Home({allPostsData}) {
     return (
         <Layout home>
-            {/* Keep the existing code here */}
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <h1 className={styles.title}>
-                Alert Page<Link href={"/alert/alert"}>alert page here!</Link>
-            </h1>
             <section className={utilStyles.headingMd}>
                 <p>[Your Self Introduction]</p>
                 <p>
@@ -32,9 +27,6 @@ export default function Home({allPostsData}) {
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
             </section>
-            */
-            {/* Add this <section> tag below the existing <section> tag */}
-
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
