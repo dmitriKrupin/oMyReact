8a2129c4e2fed2edfc4a735c6bdb18d5a1416448
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2023-08-24 14:00:10 +0300

    Third step of access has been started.

--- src/components/access/thirdStep.tsx
+++ src/components/access/thirdStep.tsx
@@ -1,14 +1,38 @@
-import { Checkbox, Option, Select, Avatar } from "@material-tailwind/react";
+import {
+  Checkbox,
+  DialogBody,
+  Dialog,
+  Avatar,
+  Card,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Typography,
+  CardHeader,
+  CardBody,
+  Button,
+  CardFooter,
+} from "@material-tailwind/react";
 import React, { useEffect } from "react";
 
 export default function ThirdStep() {
   const [exitButton, setExitButton] = React.useState("");
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     let value;
     value = localStorage.getItem("exitButton") || "";
     setExitButton(value);
   }, []);
+
+  const [arrayForCard, setArrayForCard] = React.useState({
+    id: "",
+    name: "",
+    model: "",
+    manufacture: "",
+    imageSrc: "",
+    price: "",
+  });
 
   const arrayOfButtonExit = [
     {
@@ -18,6 +42,7 @@ export default function ThirdStep() {
       manufacture: "SMARTEC",
       imageSrc:
         "https://smartec-security.com/upload/iblock/f88/ST-ER105D-GN.jpg",
+      price: "776",
     },
     {
       id: 1,
@@ -26,6 +51,7 @@ export default function ThirdStep() {
       manufacture: "AccordTec",
       imageSrc:
         "https://accordtec.ru/upload/resize_cache/iblock/d0a/el0i3mbhzo691i7g2ickvp0l3a41ixzm/2000_2000_1/0a9a87a7-51cc-11e8-9607-a4bf01347322_be384eba-f3b0-11ec-82bf-000c29832a55.jpg",
+      price: "630",
     },
     {
       id: 2,
@@ -33,6 +59,7 @@ export default function ThirdStep() {
       model: "ST-EX142",
       manufacture: "SMARTEC",
       imageSrc: "https://smartec-security.com/upload/iblock/5d9/st-ex142.jpg",
+      price: "459",
     },
     {
       id: 3,
@@ -40,6 +67,7 @@ export default function ThirdStep() {
       model: "HO-02",
       manufacture: "TANTOS",
       imageSrc: "https://tantos.pro/images/cat-items/151/ho-02_1.jpeg",
+      price: "114",
     },
   ];
 
@@ -48,32 +76,146 @@ export default function ThirdStep() {
     localStorage.setItem("exitButton", value);
   }
 
+  function openDetailByAvatar(event: any) {
+    console.log(event);
+    let model = event.target.alt;
+    getInfoForCardInput("findByAvatar", model);
+    setOpen(!open);
+  }
+
+  function openDetailById(event: any) {
+    console.log(event);
+    let id = event.target.id;
+    getInfoForCardInput("findById", id);
+    setOpen(!open);
+  }
+
+  function getInfoForCardInput(tag: string, infoFromEvent: string) {
+    let array;
+    if (tag === "findByAvatar") {
+      array = arrayOfButtonExit.find(
+        (element) => element.model === infoFromEvent
+      );
+    }
+    if (tag === "findById") {
+      array = arrayOfButtonExit.find(
+        (element) => element.id === Number(infoFromEvent)
+      );
+    }
+    setArrayForCard({
+      id: String(array?.id),
+      name: String(array?.name),
+      model: String(array?.model),
+      manufacture: String(array?.manufacture),
+      imageSrc: String(array?.imageSrc),
+      price: String(array?.price),
+    });
+  }
+
   return (
     <div className={"flex flex-col justify-center gap-4"}>
-      <div className="flex flex-row gap-6">
-        <Checkbox label="Кнопка выхода" />
-        <Select
-          label={"Выберите модель кнопки выхода:"}
-          value={exitButton}
-          onChange={(value) => {
-            saveInLocalStorage(value);
-          }}
-        >
-          {arrayOfButtonExit.map(
-            ({ id, name, model, manufacture, imageSrc }) => (
-              <Option key={id} value={manufacture}>
-                <Avatar
-                  variant="circular"
-                  alt={model}
-                  src={imageSrc}
-                  size="xs"
-                />
-                {" " + name + " || " + model + " || " + manufacture}
-              </Option>
-            )
-          )}
-        </Select>
-      </div>
+      <Card>
+        <List>
+          {arrayOfButtonExit.map(({ id, name, model, imageSrc }) => (
+            <ListItem key={id}>
+              <label
+                htmlFor={String(id)}
+                className="group/item flex flex-row items-center cursor-pointer w-full"
+                onClick={(value) => {
+                  saveInLocalStorage(value);
+                }}
+              >
+                <ListItemPrefix className="mr-3">
+                  <Checkbox
+                    id={String(id)}
+                    ripple={false}
+                    className="hover:before:opacity-0"
+                    containerProps={{
+                      className: "p-0",
+                    }}
+                  />
+                </ListItemPrefix>
+                <div className="w-full flex justify-between items-center">
+                  <Avatar
+                    variant="circular"
+                    alt={model}
+                    src={imageSrc}
+                    size="xs"
+                    onClick={openDetailByAvatar}
+                  />
+                  {" " + name + " " + model}
+                  <span className="group/edit invisible hover:bg-slate-200 group-hover/item:visible">
+                    <button
+                      className="group-hover/edit:text-blue-500 font-bold align-middle"
+                      value={id}
+                      onClick={openDetailById}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        id={String(id)}
+                        onClick={openDetailById}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          id={String(id)}
+                          d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                        />
+                      </svg>
+                    </button>
+                  </span>
+                </div>
+              </label>
+            </ListItem>
+          ))}
+        </List>
+      </Card>
+
+      <Dialog open={open} handler={() => setOpen(!open)}>
+        <DialogBody divider className="flex justify-center">
+          <Card className="w-96">
+            <CardHeader shadow={true} floated={false} className="h-96">
+              <img
+                src={arrayForCard.imageSrc}
+                alt={arrayForCard.model}
+                className="h-full w-full object-cover"
+              />
+            </CardHeader>
+            <CardBody>
+              <div className="mb-2 flex items-center justify-between">
+                <Typography color="blue-gray" className="font-medium">
+                  {arrayForCard.model}
+                </Typography>
+                <Typography color="blue-gray" className="font-medium">
+                  ₽ {arrayForCard.price}.00
+                </Typography>
+              </div>
+              <Typography
+                variant="small"
+                color="gray"
+                className="font-normal opacity-75"
+              >
+                {arrayForCard.name} {arrayForCard.manufacture}
+              </Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button
+                ripple={false}
+                fullWidth={true}
+                className="bg-blue-500 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                onClick={() => setOpen(!open)}
+              >
+                ВЕРНУТЬСЯ К ВЫБОРУ
+              </Button>
+            </CardFooter>
+          </Card>
+        </DialogBody>
+      </Dialog>
     </div>
   );
 }
