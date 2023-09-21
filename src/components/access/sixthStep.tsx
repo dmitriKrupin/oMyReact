1d82504fad3b65abe7fdc98938c530d48c7da901
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2023-09-21 15:47:19 +0300

    Input of sum for access has been updated.

--- src/components/access/sixthStep.tsx
+++ src/components/access/sixthStep.tsx
@@ -4,116 +4,169 @@ import { Checkbox, Slider } from "@material-tailwind/react";
 export default function SixthStep() {
   const [lengthOfAccess, setLengthOfAccess] = useState({
     lengthOfAccessInfo: {
-      lengthOfGofra: "",
-      lengthOfCabelCanal: "",
-      lengthOfShtrob: "",
-      lengthOfLotok: "",
-      lengthOfPodvesnoyPotok: "",
+      lengthOfGofra: 0,
+      lengthOfCabelCanal: 0,
+      lengthOfShtrob: 0,
+      lengthOfLotok: 0,
+      lengthOfPodvesnoyPotok: 0,
     },
   });
 
-  function lengthOfAccessChange(value: any, teg: String) {
-    console.log(value + " " + teg);
-
-    /*setPower({
-      ...power,
-      powerInfo: {
-        ...power.powerInfo,
-        model: value,
-      },
-    });
-    localStorage.setItem(
-      "power",
-      JSON.stringify({
-        ...power,
-        powerInfo: {
-          ...power.powerInfo,
-          model: value,
-        },
-      })
-    );*/
-  }
-
-  const [lengthOfGofra, setLengthOfGofra] = React.useState(0);
-  const [lengthOfCabelCanal, setLengthOfCabelCanal] = React.useState(0);
-  const [lengthOfShtrob, setLengthOfShtrob] = React.useState(0);
-  const [lengthOfLotok, setLengthOfLotok] = React.useState(0);
-  const [lengthOfPodvesnoyPotok, setLengthOfPodvesnoyPotok] = React.useState(0);
+  useEffect(() => {
+    let value;
+    try {
+      value = JSON.parse(localStorage.getItem("lengthOfAccess") || "");
+      setLengthOfAccess(value);
+    } catch (error: any) {
+      console.log(error);
+    }
+  }, []);
 
   const arrayOfLengthResults = [
     {
       name: "lengthOfGofra",
-      value: lengthOfGofra,
-      function: setLengthOfGofra,
+      value: lengthOfAccess.lengthOfAccessInfo.lengthOfGofra,
       label: "В гофрированной трубе 20 мм:",
       idOfSwitch: "gofra-on",
-      getLengthOfLabel: saveInLocalStorage,
     },
     {
       name: "lengthOfCabelCanal",
-      value: lengthOfCabelCanal,
-      function: setLengthOfCabelCanal,
+      value: lengthOfAccess.lengthOfAccessInfo.lengthOfCabelCanal,
       label: "В кабель-канале 40*20 мм:",
       idOfSwitch: "cabelCanal-on",
-      getLengthOfLabel: saveInLocalStorage,
     },
     {
       name: "lengthOfShtrob",
-      value: lengthOfShtrob,
-      function: setLengthOfShtrob,
+      value: lengthOfAccess.lengthOfAccessInfo.lengthOfShtrob,
       label: "Необходимо проштробить шириной 20 мм и глубиной 20 мм:",
       idOfSwitch: "shtroba-on",
-      getLengthOfLabel: saveInLocalStorage,
     },
     {
       name: "lengthOfLotok",
-      value: lengthOfLotok,
-      function: setLengthOfLotok,
+      value: lengthOfAccess.lengthOfAccessInfo.lengthOfLotok,
       label: "Проложить в существующем металлическом лотке:",
       idOfSwitch: "lotok-on",
-      getLengthOfLabel: saveInLocalStorage,
     },
     {
       name: "lengthOfPodvesnoyPotok",
-      value: lengthOfPodvesnoyPotok,
-      function: setLengthOfPodvesnoyPotok,
+      value: lengthOfAccess.lengthOfAccessInfo.lengthOfPodvesnoyPotok,
       label: "Проложить за существующим подвесным потолком типа Armstrong:",
       idOfSwitch: "podvesnoyPotok-on",
-      getLengthOfLabel: saveInLocalStorage,
     },
   ];
 
-  useEffect(() => {
-    for (let i = 0; i < arrayOfLengthResults.length; i++) {
-      let value;
-      value = localStorage.getItem(arrayOfLengthResults[i].name) || "0";
-      arrayOfLengthResults[i].value = parseInt(value);
-      arrayOfLengthResults[i].function(parseInt(value));
-    }
-  });
-
-  let sumLengthOfCables =
-    Number(lengthOfGofra) +
-    Number(lengthOfCabelCanal) +
-    Number(lengthOfShtrob) +
-    Number(lengthOfLotok) +
-    Number(lengthOfPodvesnoyPotok);
-
+  //todo:переделать метод с циклом для уменьшения кода
   function saveInLocalStorage(event: any, name: string) {
-    let value = event.target.value;
-    arrayOfLengthResults.map((entry) => {
-      if (entry.name === name) {
-        entry.function(value);
-        localStorage.setItem(entry.name, value);
-      }
-    });
+    let value = Number(event.target.value);
+    if (name === "lengthOfGofra") {
+      setLengthOfAccess({
+        ...lengthOfAccess,
+        lengthOfAccessInfo: {
+          ...lengthOfAccess.lengthOfAccessInfo,
+          lengthOfGofra: value,
+        },
+      });
+      localStorage.setItem(
+        "lengthOfAccess",
+        JSON.stringify({
+          ...lengthOfAccess,
+          lengthOfAccessInfo: {
+            ...lengthOfAccess.lengthOfAccessInfo,
+            lengthOfGofra: value,
+          },
+        })
+      );
+    }
+    if (name === "lengthOfCabelCanal") {
+      setLengthOfAccess({
+        ...lengthOfAccess,
+        lengthOfAccessInfo: {
+          ...lengthOfAccess.lengthOfAccessInfo,
+          lengthOfCabelCanal: value,
+        },
+      });
+      localStorage.setItem(
+        "lengthOfAccess",
+        JSON.stringify({
+          ...lengthOfAccess,
+          lengthOfAccessInfo: {
+            ...lengthOfAccess.lengthOfAccessInfo,
+            lengthOfCabelCanal: value,
+          },
+        })
+      );
+    }
+    if (name === "lengthOfShtrob") {
+      setLengthOfAccess({
+        ...lengthOfAccess,
+        lengthOfAccessInfo: {
+          ...lengthOfAccess.lengthOfAccessInfo,
+          lengthOfShtrob: value,
+        },
+      });
+      localStorage.setItem(
+        "lengthOfAccess",
+        JSON.stringify({
+          ...lengthOfAccess,
+          lengthOfAccessInfo: {
+            ...lengthOfAccess.lengthOfAccessInfo,
+            lengthOfShtrob: value,
+          },
+        })
+      );
+    }
+    if (name === "lengthOfLotok") {
+      setLengthOfAccess({
+        ...lengthOfAccess,
+        lengthOfAccessInfo: {
+          ...lengthOfAccess.lengthOfAccessInfo,
+          lengthOfLotok: value,
+        },
+      });
+      localStorage.setItem(
+        "lengthOfAccess",
+        JSON.stringify({
+          ...lengthOfAccess,
+          lengthOfAccessInfo: {
+            ...lengthOfAccess.lengthOfAccessInfo,
+            lengthOfLotok: value,
+          },
+        })
+      );
+    }
+    if (name === "lengthOfPodvesnoyPotok") {
+      setLengthOfAccess({
+        ...lengthOfAccess,
+        lengthOfAccessInfo: {
+          ...lengthOfAccess.lengthOfAccessInfo,
+          lengthOfPodvesnoyPotok: value,
+        },
+      });
+      localStorage.setItem(
+        "lengthOfAccess",
+        JSON.stringify({
+          ...lengthOfAccess,
+          lengthOfAccessInfo: {
+            ...lengthOfAccess.lengthOfAccessInfo,
+            lengthOfPodvesnoyPotok: value,
+          },
+        })
+      );
+    }
   }
+
+  let sum =
+    Number(lengthOfAccess.lengthOfAccessInfo.lengthOfGofra) +
+    Number(lengthOfAccess.lengthOfAccessInfo.lengthOfCabelCanal) +
+    Number(lengthOfAccess.lengthOfAccessInfo.lengthOfShtrob) +
+    Number(lengthOfAccess.lengthOfAccessInfo.lengthOfLotok) +
+    Number(lengthOfAccess.lengthOfAccessInfo.lengthOfPodvesnoyPotok);
 
   return (
     <div className={"h-full flex flex-col"}>
       <div className={"flex flex-row justify-between font-bold"}>
         <div>Общая длина трасс:</div>
-        <div className={"text-blue-500 font-bold"}>{sumLengthOfCables}</div>
+        <div className={"text-blue-500 font-bold"}>{sum}</div>
       </div>
       {arrayOfLengthResults.map((entry) => (
         <div
@@ -148,7 +201,7 @@ export default function SixthStep() {
                 defaultValue={entry.value}
                 value={entry.value}
                 onChange={(event) => {
-                  entry.getLengthOfLabel(event, entry.name);
+                  saveInLocalStorage(event, entry.name);
                 }}
               />
             </div>
