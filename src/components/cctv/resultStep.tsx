84db99f4155fda11688a6e9f639a324b657b73ee
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2023-09-25 18:19:58 +0300

    Result page of CCTV has been updated.

--- src/components/cctv/resultStep.tsx
+++ src/components/cctv/resultStep.tsx
@@ -3,78 +3,96 @@ import React from "react";
 import { PencilIcon } from "@heroicons/react/20/solid";
 
 const TABLE_HEAD = [
-    "№",
-    "Количество камер",
-    "Длина трасс",
-    "Статус",
-    "Оборудование иматериалы",
-    "Работы",
-    "ИТОГО"
+  {
+    number: "№",
+    cameras: "Количество камер",
+    length: "Длина трасс",
+    status: "Статус",
+    equipment: "Оборудование и материалы",
+    work: "Работы",
+    total: "ИТОГО",
+  },
 ];
 
 function editButton() {
-    return (
-        <Tooltip content="Редактировать расчет">
-            <IconButton variant="text" color="blue-gray">
-                <PencilIcon className="h-4 w-4" />
-            </IconButton>
-        </Tooltip>
-    );
+  return (
+    <Tooltip content="Редактировать расчет">
+      <IconButton variant="text" color="blue-gray">
+        <PencilIcon className="h-4 w-4" />
+      </IconButton>
+    </Tooltip>
+  );
 }
 
 export default function ResultStep() {
-    const TABLE_BODY = [
-        {
-            id: 1,
-            cameras: localStorage.getItem('numberOfCameras') || '0',
-            length: getSumOfLength(),
-            status: "Черновик",
-            costOfEquipment: "Оборудование",
-            costOfWork: "Работы",
-            total: "ИТОГО",
-            button: editButton,
-        }
-    ];
+  const TABLE_BODY = [
+    {
+      id: 1,
+      cameras: localStorage.getItem("numberOfCameras") || "0",
+      length: getSumOfLength(),
+      status: "Черновик",
+      costOfEquipment: "Оборудование",
+      costOfWork: "Работы",
+      total: "ИТОГО",
+      button: editButton,
+    },
+  ];
 
-    function getSumOfLength() {
-        let lengthOfGofra = Number(localStorage.getItem('lengthOfGofra'));
-        let lengthOfCabelCanal = Number(localStorage.getItem('lengthOfCabelCanal'));
-        let lengthOfShtrob = Number(localStorage.getItem('lengthOfShtrob'));
-        let lengthOfLotok = Number(localStorage.getItem('lengthOfLotok'));
-        let lengthOfPodvesnoyPotok = Number(localStorage.getItem('lengthOfPodvesnoyPotok'));
-        return (
-            lengthOfGofra + lengthOfCabelCanal + lengthOfShtrob + lengthOfLotok + lengthOfPodvesnoyPotok
-        );
-    }
-
-    return (
-        //todo: добавить готовый результат с указанием стоимостей за работы и необходимые оборудование и материалы
-        <div className="result-table">
-            <table>
-                <thead>
-                    <tr>
-                        {TABLE_HEAD.map((entry) => (
-                            <th key={entry}>
-                                {entry}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {TABLE_BODY.map((entry) => (
-                        <tr key={entry.id}>
-                            <th>{entry.id}</th>
-                            <th>{entry.cameras}</th>
-                            <th>{entry.length}</th>
-                            <th>{entry.status}</th>
-                            <th>{entry.costOfEquipment}</th>
-                            <th>{entry.costOfWork}</th>
-                            <th>{entry.total}</th>
-                            <th>{entry.button()}</th>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+  function getSumOfLength() {
+    let lengthOfGofra = Number(localStorage.getItem("lengthOfGofra"));
+    let lengthOfCabelCanal = Number(localStorage.getItem("lengthOfCabelCanal"));
+    let lengthOfShtrob = Number(localStorage.getItem("lengthOfShtrob"));
+    let lengthOfLotok = Number(localStorage.getItem("lengthOfLotok"));
+    let lengthOfPodvesnoyPotok = Number(
+      localStorage.getItem("lengthOfPodvesnoyPotok")
     );
+    return (
+      lengthOfGofra +
+      lengthOfCabelCanal +
+      lengthOfShtrob +
+      lengthOfLotok +
+      lengthOfPodvesnoyPotok
+    );
+  }
+
+  return (
+    //todo: добавить готовый результат с указанием стоимостей за работы и необходимые оборудование и материалы
+    <div className="result-table">
+      <table>
+        <thead>
+          {TABLE_HEAD.map((entry) => (
+            <tr
+              key={entry.number}
+              className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-8 gap-4"
+            >
+              <th>{entry.number}</th>
+              <th>{entry.cameras}</th>
+              <th>{entry.length}</th>
+              <th className="hidden sm:grid">{entry.status}</th>
+              <th className="hidden md:grid">{entry.equipment}</th>
+              <th className="hidden md:grid">{entry.work}</th>
+              <th>{entry.total}</th>
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {TABLE_BODY.map((entry) => (
+            <tr
+              key={entry.id}
+              className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-8 gap-4 items-center"
+            >
+              <th>{entry.id}</th>
+              <th>{entry.cameras}</th>
+              <th>{entry.length}</th>
+              <th className="hidden sm:grid">{entry.status}</th>
+              <th className="hidden md:grid">{entry.costOfEquipment}</th>
+              <th className="hidden md:grid">{entry.costOfWork}</th>
+              <th>{entry.total}</th>
+              <th>{entry.button()}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
